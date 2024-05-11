492f81829d01948363c96ce5d2fae80268145a74
DivyaSai <divyasaikandula@gmail.com>
2024-05-11 16:27:28 +0530

    music player

---
+++ js/music-list.js
@@ -0,0 +1,41 @@
+
+
+let allMusic = [
+  {
+    name: "Raanjhanaa",
+    artist: "Dhanush, Sonam Kapoor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Style",
+    artist: "PrabhuDeva,Lawrence",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Orange",
+    artist: "RamCharan,Genelia",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Darling",
+    artist: "Prabhas,Kajal",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Gabbar Singh",
+    artist: "PawanKalyan,SruthiHasan",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Akhil",
+    artist: "Akhil, Sayyeshaa Saigal",
+    img: "music-6",
+    src: "music-6"
+  },
+  
+];
